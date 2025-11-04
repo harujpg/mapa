acf2ee594ba6cd807041783d7c518c992f4cc2ad
harujpg <127435584+harujpg@users.noreply.github.com>
2025-11-04 16:34:57 -0300

    Update Supabase URL and key in configuration

--- config/supabase.js
+++ config/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://lyoakfcebclmglqohmku.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5b2FrZmNlYmNsbWdscW9obWt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2NzQ5NTUsImV4cCI6MjA3NzI1MDk1NX0.mY6nuimRcXm8Aop7_Z4ZKX47qvqUqHKpVYenncOXO0o'
+const supabaseUrl = 'https://vhrbapnoppdwrhxajfpg.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZocmJhcG5vcHBkd3JoeGFqZnBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyNzQwMTIsImV4cCI6MjA3Nzg1MDAxMn0.BG4b4lNnkCjD3D93rXEplMTvPsbb_iCBW9Wh_NhZ_Sw'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
